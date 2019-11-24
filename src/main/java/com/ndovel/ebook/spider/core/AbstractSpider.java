278c2f3a9b2c24bf68e3278a657ec07ab39a942f
graydovee <1036308853@qq.com>
2019-11-24 16:51:29 +0800

    fix some bug

--- src/main/java/com/ndovel/ebook/spider/core/AbstractSpider.java
+++ src/main/java/com/ndovel/ebook/spider/core/AbstractSpider.java
@@ -7,18 +7,25 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class AbstractSpider {
+    private final static int RETRY_TIMES = 3;
+    private final static int RETRY_INTERVAL = 1;
+
     private String getFullPath(String url) {
         return UrlUtils.urlFormat(url);
     }
 
     protected String sendHttpGetRequest(String url) {
-        int num = 3;
-        for (int i = 1; i <= num; ++i) {
+        for (int i = 1; i <= RETRY_TIMES; ++i) {
             try {
                 return HttpClientUtils.get(getFullPath(url));
             } catch (RequestException e) {
                 if (i < 3) {
-                    log.warn("ready to the " + (3 - num) + "th retry!");
+                    log.warn("the " + i + "th retry will started after " + RETRY_INTERVAL +"s");
+                    try {
+                        Thread.sleep(RETRY_INTERVAL * 1000);
+                    } catch (InterruptedException ex) {
+                        log.error("sleep error");
+                    }
                 } else {
                     log.error(e.getMessage(), e);
                 }
